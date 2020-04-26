89f0751669f3ff48db1492938249f890236e4509
Gabor Csomak Opal <csomakk@gmail.com>
2020-04-26 12:11:25 +0200

    first commit

---
+++ project.ino
@@ -0,0 +1,78 @@
+#include <SPI.h>
+#include <WiFi101.h>
+#include "arduino_secrets.h" 
+
+char ssid[] = "CsMi";
+char pass[] = "0123456789";
+int keyIndex = 0;
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+bool alreadyConnected = false;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to network, SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+
+  server.begin();
+  Serial.print("Connected to wifi. My address:");
+  IPAddress myAddress = WiFi.localIP();
+  Serial.println(myAddress);
+
+  
+}
+
+void loop() {
+ // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    bool currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n' && currentLineIsBlank) {
+        //  client.println("arduino got it");
+        }
+        
+      }
+      delay(10);
+    }
+    // give the web browser time to receive the data
+    delay(10);
+  }
+}
+
+
+void printWiFiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
